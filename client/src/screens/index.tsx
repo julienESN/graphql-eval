92d177d00a77102f569fe588c94b35af1b742451
Sofiane Fares <sofiane.fares77@outlook.fr>
2025-02-04 17:14:11 +0100

    Base du front
    implémentation de vite avec react
    Implémentation de tailwind ainsi que react-router-dom.
    Implémentation de shadcn/ui.

---
+++ client/src/screens/index.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button.tsx";
+
+export default function IndexScreen() {
+    return (<>
+            <a className={"text-red-400"}>Hello World</a>
+
+            <Button>Click me</Button>
+
+        </>
+    )
+}
