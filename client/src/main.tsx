d55640ca1493f4468333d1a318a2639349643894
Sofiane Fares <sofiane.fares77@outlook.fr>
2025-02-05 09:19:34 +0100

    Ajout de la page Login Page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
 import IndexScreen from "./screens/index";
+import LoginPage from "./screens/LoginPage"
 import {BrowserRouter, Route, Routes} from "react-router";
 
 createRoot(document.getElementById('root')!).render(
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<IndexScreen/>}/>
+        <Route path={"/login"} element={<LoginPage/>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
