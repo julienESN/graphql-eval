4f1b3a4bf190972c933110a51a153f25b05a8b9f
Sofiane Fares <sofiane.fares77@outlook.fr>
2025-02-05 10:58:40 +0100

    add .env to gitignore

--- client/src/screens/index.tsx
+++ client/src/screens/index.tsx
@@ -1,13 +1,16 @@
-/**
- * Page d'accueil de l'application
- * @file index.tsx
- */
+import {useAuth} from "@/context/AuthContext";
 
-export default function IndexScreen() {
+const IndexScreen: React.FC = () => {
+    const {user} = useAuth();
+
+    console.log(user);
+
     return (
         <div className="p-6">
             <h1 className="text-2xl font-bold mb-4">Bienvenue sur MonApp</h1>
             <p>Contenu de la page d'accueil...</p>
         </div>
     );
-}
+};
+
+export default IndexScreen;
