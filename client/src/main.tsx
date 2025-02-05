ba6a329dcf1ca6fa099ef57721087f796fbdf5cb
Sofiane Fares <sofiane.fares77@outlook.fr>
2025-02-05 10:02:33 +0100

    Ajout de la logique pour la connexion/inscription et de la connexion a l'API

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,15 +3,34 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import IndexScreen from "./screens/index";
 import LoginPage from "./screens/LoginPage"
+import SignUpPage from "./screens/SignUpPage"
 import {BrowserRouter, Route, Routes} from "react-router";
+import {ApolloProvider} from "@apollo/client";
+import client from './apolloClient';
+import {AuthProvider} from "@/context/AuthContext.tsx";
+import ProtectedRoute from "@/components/ProtectedRoute.tsx"; // Chemin vers votre Apollo Client
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<IndexScreen/>}/>
-        <Route path={"/login"} element={<LoginPage/>} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <IndexScreen/>
+                </ProtectedRoute>
+              }
+            />
+            <Route path={"/login"} element={<LoginPage/>}/>
+            <Route path={"/signup"} element={<SignUpPage/>}/>
+
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+
+    </ApolloProvider>
   </StrictMode>,
 )
